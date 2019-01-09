4b73abbf0bc1d0b0225501083d653b872467f92d
Pine Wu <octref@gmail.com>
2019-01-09 13:56:46 -0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Code
+
+Adapted from https://github.com/Microsoft/vscode-extension-samples/tree/master/helloworld-sample for my own use.
---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World!');
+		})
+	)
+}
+
+export function deactivate() {}
