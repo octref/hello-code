96fafb3bcf95862ca49a9dc5bf3f627a700a573b
Pine Wu <octref@gmail.com>
2019-03-31 09:14:56 -0700

    lint-staged with prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.helloWorld', () => {
-			vscode.window.showInformationMessage('Hello World!');
-		})
-	)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.helloWorld', () => {
+      vscode.window.showInformationMessage('Hello World!')
+    })
+  )
 }
 
 export function deactivate() {}
