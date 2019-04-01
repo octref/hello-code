2eefb1a29cd66bd758c1842f9a6f6d9ee9eaa5de
Pine Wu <octref@gmail.com>
2019-04-01 11:23:53 -0700

    Unused bug

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,8 @@ import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('extension.helloWorld', () => {
-      vscode.window.showInformationMessage('Hello World!')
+      const diags = vscode.languages.getDiagnostics(vscode.window.activeTextEditor.document.uri)
+      console.log(JSON.stringify(diags, null, 2))
     })
   )
 }
